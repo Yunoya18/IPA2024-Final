f52feaa5571098bb660e209b63e8eb08c760eb1b
Yunoya <sonrasamon15725@gmail.com>
2025-10-23 15:21:40 +0700

    update ansible hosts to make it work

--- ansible_final.py
+++ ansible_final.py
@@ -2,7 +2,7 @@ import subprocess
 
 def showrun():
     # read https://www.datacamp.com/tutorial/python-subprocess to learn more about subprocess
-    command = ['ansible-playbook', 'backup_playbook.yaml']
+    command = ['ansible-playbook', 'backup_playbook.yaml', '-i', 'hosts']
     result = subprocess.run(command, capture_output=True, text=True)
     result = result.stdout
     if 'ok=2' in result:
